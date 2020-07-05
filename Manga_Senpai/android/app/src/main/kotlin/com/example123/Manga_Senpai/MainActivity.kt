47402cab541ab0cba4a5462880584339265936e0
JP-2000 <jitenpatel1482000@gmail.com>
2020-07-05 17:57:00 +0530

    Setup Complete along with firebase plugins  (Errors Removed)

---
+++ Manga_Senpai/android/app/src/main/kotlin/com/example123/Manga_Senpai/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example123.Manga_Senpai
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ Manga_Senpai/lib/main.dart
@@ -0,0 +1,14 @@
+//import 'package:cloud_firestore/cloud_firestore.dart';
+//import 'package:firebase_core_web/firebase_core_web.dart';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'exp.dart';
+
+void main() => runApp(
+  new MaterialApp(
+    theme: ThemeData.dark(),
+    home: Exp(),
+  )
+);
